b3398826aa8a26d47eb448cc4f951f2c9b45cd34
Redouane Achouri <achouri.a.r@gmail.com>
2020-02-22 20:22:43 +0100

    Added Tokenizer and Lemmatization functions for pre-processing tweets.

--- consume-tweets/run.py
+++ consume-tweets/run.py
@@ -13,24 +13,24 @@ if __name__ == "__main__":
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     CONFIG_PATH = os.path.join(ROOT_DIR, 'config.ini')
 
-    config = configparser.ConfigParser(strict=True)
-    config.read_file(open(CONFIG_PATH, 'r'))
+    # config = configparser.ConfigParser(strict=True)
+    # config.read_file(open(CONFIG_PATH, 'r'))
 
     try:
-        logging.info("Connecting to Kafka topic '{}'@'{}'"
-            .format(config['kafka'].get('topic'), config['kafka'].get('broker')))
-
-        consumer = StreamProcess(
-            config['kafka'].get('topic'),
-            bootstrap_servers = config['kafka'].get('broker'),
-            enable_auto_commit = True,
-            auto_offset_reset = 'latest')
+        # logging.info("Connecting to Kafka topic '{}'@'{}'"
+        #     .format(config['kafka'].get('topic'), config['kafka'].get('broker')))
 
         # consumer = StreamProcess(
-        #     'tweets',
-        #     bootstrap_servers = 'localhost:9092',
+        #     config['kafka'].get('topic'),
+        #     bootstrap_servers = config['kafka'].get('broker'),
         #     enable_auto_commit = True,
         #     auto_offset_reset = 'latest')
+
+        consumer = StreamProcess(
+            'tweets',
+            bootstrap_servers = 'localhost:9092',
+            enable_auto_commit = True,
+            auto_offset_reset = 'latest')
 
     except NoBrokersAvailable as e:
         logging.error("No brokers found at '{}'.".format(config['kafka'].get('broker')))
--- consume-tweets/stream_process.py
+++ consume-tweets/stream_process.py
@@ -1,11 +1,24 @@
 import logging
+import re, string
 
 from kafka import KafkaConsumer
+from nltk.tag import pos_tag
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import TweetTokenizer
 
 class StreamProcess(KafkaConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.broker = kwargs['bootstrap_servers']
+        self.stopwords = stopwords.words('english')
+
+        self._word_tokenizer = TweetTokenizer(
+            preserve_case=True,
+            reduce_len=False,
+            strip_handles=False)
+
+        self._lemmatizer = WordNetLemmatizer()
 
     def process(self):
         try:
@@ -13,9 +26,44 @@ class StreamProcess(KafkaConsumer):
             tweet = message.value.decode('utf-8').strip()
 
             print(tweet)
+            print(self._lemmatize(self._tokenize(tweet)))
             print('=' * 50)
             # logging.info(message.offset)
 
         except StopIteration as e:
             logging.warning("No incoming message found at Kafka broker: {}.".format(self.broker))
             return
+
+    def _tokenize(self, t):
+        return self._word_tokenizer.tokenize(t)
+
+    def _is_noise(self, word):
+        pattern = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+|(@[A-Za-z0-9_]+)'
+        return word in string.punctuation \
+            or word.lower() in self.stopwords \
+            or re.search(pattern, word, re.IGNORECASE) != None
+
+    def _tag2type(self, tag):
+        """
+        Take a tag and return a type
+        Common tags are:
+            - NNP: Noun, proper, singular
+            - NN: Noun, common, singular or mass
+            - IN: Preposition or conjunction, subordinating
+            - VBG: Verb, gerund or present participle
+            - VBN: Verb, past participle
+
+        return 'n' for noun, 'v' for verb, and 'a' for any
+        """
+        if tag.startswith('NN'):
+            return 'n'
+        elif tag.startswith('VB'):
+            return 'v'
+        else:
+            return 'a'
+
+    def _lemmatize(self, tokens):
+        return [
+            self._lemmatizer.lemmatize(word, self._tag2type(tag)).lower()
+            for word, tag in pos_tag(tokens) if not self._is_noise(word)
+        ]
